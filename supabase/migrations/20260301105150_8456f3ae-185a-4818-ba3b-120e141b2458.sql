35014733265976facbeebc342d58e3cbbfd44def
cassioaraujo22-creator <cassioaraujo22@gmail.com>
2026-03-27 09:15:01 -0300

    delete
    delete

--- supabase/migrations/20260301105150_8456f3ae-185a-4818-ba3b-120e141b2458.sql
+++
@@ -1,3 +0,0 @@
-
-ALTER PUBLICATION supabase_realtime ADD TABLE public.assigned_workouts;
-ALTER PUBLICATION supabase_realtime ADD TABLE public.workout_sessions;
--- supabase/migrations/20260320145331_9bf272dc-7f52-4dca-a3e6-599463e5764c.sql
+++
@@ -1,6 +0,0 @@
-CREATE POLICY "Owners update own gym"
-ON public.gyms
-FOR UPDATE
-TO authenticated
-USING (has_gym_role(auth.uid(), id, 'owner'::app_role))
-WITH CHECK (has_gym_role(auth.uid(), id, 'owner'::app_role));
